65e634efdd8954147b00ebefcc352e6cb46b9b7f
Deme Sibere <98864310+DemeSibere@users.noreply.github.com>
2022-05-10 22:50:53 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const htmlRoutes = require('./02-Homework/Develop/routes/htmlRoutes');
+const express = require('express');
+const app = express();
+const apiRoutes = require('./02-Homework/Develop/routes/apiRoutes');
+const PORT = process.env.PORT || 3000;
+
+// Folder to retrieve CSS and JS Files
+
+
+// Middleware to parse the JSON data
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static("public"));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// PORT
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+});
+
+module.exports = app;
